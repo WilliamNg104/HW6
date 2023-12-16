512e7fa031fb3c827702cad0bec3bc2c9e4a54ad
WilliamNg104 <114209234+WilliamNg104@users.noreply.github.com>
2023-12-17 00:44:37 +0800

    Add files via upload

---
+++ P10-7/P10-7/main.c
@@ -0,0 +1,34 @@
+//
+//  main.c
+//  P10-7
+//
+//  Created by William Ng on 2023/12/17.
+//
+
+/* Fig. 10.2: fig10_02.c
+   Using the structure member and
+   structure pointer operators */
+
+#include <stdio.h>
+
+/* card structure definition */
+struct card {
+    char *face; /* define pointer face */
+    char *suit; /* define pointer siut */
+}; /* end structure card */
+
+int main(void)
+{
+    struct card aCard; /* define one struct card variable */
+    struct card *cardPtr; /* deine a pointer to a struct card */
+    
+    /* place strings into aCard */
+    aCard.face = "Ace";
+    aCard.suit = "Spades";
+    
+    cardPtr = &aCard; /* assign address of aCard to cardPtr */
+    printf( "%s%s%s\n%s%s%s\n%s%s%s\n" , aCard.face, " of ", aCard.suit,
+           cardPtr->face, " of ", cardPtr->suit,
+           ( *cardPtr ).face, " of ", ( *cardPtr ).suit );
+    return 0; /*indicates successful termination */
+} /* end main */
